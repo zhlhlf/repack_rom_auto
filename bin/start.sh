7825a2d56affc793e97c4160c4cff8adbf955872
zhlhlf <zhlhlf@gmail.com>
2024-06-25 09:30:05 +0800

    a

--- bin/start.sh
+++ bin/start.sh
@@ -175,7 +175,7 @@ repack_img "portrom/images/boot"
 # 打包各镜像img
 for pname in ${super_list};do
     if [ -d "portrom/images/$pname" ];then
-        repack_img "portrom/images/$pname"
+        repack_img "portrom/images/$pname" $pack_type
     fi
 done
 
