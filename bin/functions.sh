c4a73185a8e63fd0ceb92737f12629a4a22f1d78
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-08-24 18:30:59 +0800

    Update functions.sh

--- bin/functions.sh
+++ bin/functions.sh
@@ -867,7 +867,7 @@ get_rom_msg(){
 
     base_android_version=$(< $1/my_product/build.prop grep "ro.build.version.oplusrom" |awk 'NR==1' |cut -d '=' -f 2)
     base_android_sdk=$(< $1/system/system/build.prop grep "ro.system.build.version.sdk" |awk 'NR==1' |cut -d '=' -f 2)
-    base_rom_version=$(<  $1/my_manifest/build.prop grep "ro.build.display.id" | awk 'NR==1' | cut -d '=' -f 2)
+    base_rom_version=$(<  $1/my_manifest/build.prop grep "ro.build.display.id" | awk 'NR==1' | cut -d '=' -f 2 | cut -d '_' -f 2-)
     base_device_code=$(< $1/my_manifest/build.prop grep "ro.oplus.version.my_manifest" | awk 'NR==1' | cut -d '=' -f 2 | cut -d "_" -f 1)
     base_product_device=$(< $1/my_manifest/build.prop grep "ro.product.device" |awk 'NR==1' |cut -d '=' -f 2)
     base_product_name=$(< $1/my_manifest/build.prop grep "ro.product.name" |awk 'NR==1' |cut -d '=' -f 2)
@@ -891,7 +891,7 @@ get_rom_msg(){
 
     port_android_version=$(< $2/my_product/build.prop grep "ro.build.version.oplusrom" |awk 'NR==1' |cut -d '=' -f 2)
     port_android_sdk=$(< $2/system/system/build.prop grep "ro.system.build.version.sdk" |awk 'NR==1' |cut -d '=' -f 2)
-    port_rom_version=$(<  $2/my_manifest/build.prop grep "ro.build.display.id" | awk 'NR==1' | cut -d '=' -f 2)
+    port_rom_version=$(<  $2/my_manifest/build.prop grep "ro.build.display.id" | awk 'NR==1' | cut -d '=' -f 2 | cut -d '_' -f 2-)
     port_device_code=$(< $2/my_manifest/build.prop grep "ro.oplus.version.my_manifest" | awk 'NR==1' | cut -d '=' -f 2 | cut -d "_" -f 1)
     port_product_device=$(< $2/my_manifest/build.prop grep "ro.product.device" |awk 'NR==1' |cut -d '=' -f 2)
     port_product_name=$(< $2/my_manifest/build.prop grep "ro.product.name" |awk 'NR==1' |cut -d '=' -f 2)
@@ -909,7 +909,7 @@ get_rom_msg(){
     green "Product Name: BASEROM: [${base_product_name}], PORTROM: [${port_product_name}]"
     green "Market Name: BASEROM: [${base_market_name}], PORTROM: [${port_market_name}]"
     green "Product Model: BASEROM: [${base_rom_model}], PORTROM: [${port_rom_model}]"
-}
+ }
 
 #$1 全局路径
 change_device_buildProp(){
